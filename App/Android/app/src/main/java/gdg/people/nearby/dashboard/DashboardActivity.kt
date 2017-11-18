761d525e1d8070aad6dbe5d17cebbf4961ee84b5
Goddchen <goddchen@gmail.com>
2017-11-18 15:07:23 +0100

    move Nearby code to DashboardActivity.kt

--- App/Android/app/src/main/java/gdg/people/nearby/dashboard/DashboardActivity.kt
+++ App/Android/app/src/main/java/gdg/people/nearby/dashboard/DashboardActivity.kt
@@ -1,13 +1,47 @@
 package gdg.people.nearby.dashboard
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.connection.*
+import com.google.gson.Gson
 import gdg.people.nearby.R
+import gdg.people.nearby.model.Person
 
 import kotlinx.android.synthetic.main.dashboard.*
+import timber.log.Timber
 
 class DashboardActivity : AppCompatActivity() {
+
+    var connectionLifecycleCallback = object : ConnectionLifecycleCallback() {
+        override fun onConnectionResult(p0: String?, p1: ConnectionResolution?) {
+            Timber.d("Connection result: %s, connectionResolution: %s", p0, p1)
+        }
+
+        override fun onDisconnected(p0: String?) {
+            Timber.d("Disconnected: %s", p0)
+        }
+
+        override fun onConnectionInitiated(p0: String?, p1: ConnectionInfo?) {
+            Timber.d("Connection initiated: %s, info: %s", p0, p1)
+        }
+    }
+
+    var endpointDiscoveryCallback = object : EndpointDiscoveryCallback() {
+        override fun onEndpointFound(p0: String?, p1: DiscoveredEndpointInfo?) {
+            Timber.d("Endpoint found: %s, info: %s", p0, p1)
+            val foundPerson: Person = Gson().fromJson(p0, Person::class.java)
+            Timber.d("Found person: %s", foundPerson)
+        }
+
+        override fun onEndpointLost(p0: String?) {
+            Timber.d("Endpoint lost: %s", p0)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +52,51 @@ class DashboardActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
+
+
+    private fun startNearby() {
+        val me: Person? = null
+        Nearby.getConnectionsClient(this)
+                .startAdvertising(
+                        Gson().toJson(me),
+                        packageName,
+                        connectionLifecycleCallback,
+                        AdvertisingOptions(Strategy.P2P_CLUSTER))
+                .addOnSuccessListener { Timber.d("Advertising started") }
+                .addOnFailureListener { Timber.e(it) }
+        Nearby.getConnectionsClient(this)
+                .startDiscovery(packageName,
+                        endpointDiscoveryCallback,
+                        DiscoveryOptions(Strategy.P2P_CLUSTER))
+                .addOnSuccessListener { Timber.d("Discovery started") }
+                .addOnFailureListener { Timber.e(it) }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION),
+                    0)
+        } else {
+            startNearby()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when {
+            grantResults.all { it == PackageManager.PERMISSION_GRANTED } -> startNearby()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Nearby.getConnectionsClient(this).stopAdvertising()
+        Nearby.getConnectionsClient(this).stopDiscovery()
     }
 
 }
--- App/Android/app/src/main/java/gdg/people/nearby/welcome/WelcomeActivity.kt
+++ App/Android/app/src/main/java/gdg/people/nearby/welcome/WelcomeActivity.kt
@@ -1,44 +1,13 @@
 package gdg.people.nearby.welcome
 
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import com.google.android.gms.nearby.Nearby
-import com.google.android.gms.nearby.connection.*
 import gdg.people.nearby.R
 import gdg.people.nearby.dashboard.DashboardActivity
-import timber.log.Timber
-import java.util.*
 
 class WelcomeActivity : AppCompatActivity() {
-
-    var connectionLifecycleCallback = object : ConnectionLifecycleCallback() {
-        override fun onConnectionResult(p0: String?, p1: ConnectionResolution?) {
-            Timber.d("Connection result: %s, connectionResolution: %s", p0, p1)
-        }
-
-        override fun onDisconnected(p0: String?) {
-            Timber.d("Disconnected: %s", p0)
-        }
-
-        override fun onConnectionInitiated(p0: String?, p1: ConnectionInfo?) {
-            Timber.d("Connection initiated: %s, info: %s", p0, p1)
-        }
-    }
-
-    var endpointDiscoveryCallback = object : EndpointDiscoveryCallback() {
-        override fun onEndpointFound(p0: String?, p1: DiscoveredEndpointInfo?) {
-            Timber.d("Endpoint found: %s, info: %s", p0, p1)
-        }
-
-        override fun onEndpointLost(p0: String?) {
-            Timber.d("Endpoint lost: %s", p0)
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,48 +17,5 @@ class WelcomeActivity : AppCompatActivity() {
         findPeopleNearby.setOnClickListener {
             startActivity(Intent(this, DashboardActivity::class.java))
         }
-    }
-
-    fun startNearby() {
-        Nearby.getConnectionsClient(this)
-                .startAdvertising(
-                        UUID.randomUUID().toString(),
-                        packageName,
-                        connectionLifecycleCallback,
-                        AdvertisingOptions(Strategy.P2P_CLUSTER))
-                .addOnSuccessListener { Timber.d("Advertising started") }
-                .addOnFailureListener { Timber.e(it) }
-        Nearby.getConnectionsClient(this)
-                .startDiscovery(packageName,
-                        endpointDiscoveryCallback,
-                        DiscoveryOptions(Strategy.P2P_CLUSTER))
-                .addOnSuccessListener { Timber.d("Discovery started") }
-                .addOnFailureListener { Timber.e(it) }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if (ContextCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION),
-                    0)
-        } else {
-            startNearby()
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when {
-            grantResults.all { it == PackageManager.PERMISSION_GRANTED } -> startNearby()
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Nearby.getConnectionsClient(this).stopAdvertising()
-        Nearby.getConnectionsClient(this).stopDiscovery()
     }
 }
