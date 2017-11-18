9f153e67c554df4cc2336f05a84a4f98b47b805a
Marc Reichelt <mcreichelt@gmail.com>
2017-11-18 16:47:34 +0100

    add debug code for person activity

--- App/Android/app/src/main/java/gdg/people/nearby/person/PersonActivity.kt
+++ App/Android/app/src/main/java/gdg/people/nearby/person/PersonActivity.kt
@@ -3,8 +3,9 @@ package gdg.people.nearby.person
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import com.google.gson.Gson
 import gdg.people.nearby.R
-
+import gdg.people.nearby.model.Person
 import kotlinx.android.synthetic.main.person.*
 
 class PersonActivity : AppCompatActivity() {
@@ -13,6 +14,11 @@ class PersonActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.person)
         setSupportActionBar(toolbar)
+
+        val json = intent.getStringExtra("person")
+        val person: Person = Gson().fromJson(json, Person::class.java)
+
+        title = person.name
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
--- App/Android/app/src/main/java/gdg/people/nearby/welcome/WelcomeActivity.kt
+++ App/Android/app/src/main/java/gdg/people/nearby/welcome/WelcomeActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
+import com.google.gson.Gson
 import gdg.people.nearby.R
 import gdg.people.nearby.dashboard.DashboardActivity
 import gdg.people.nearby.helper.Preferences
 import gdg.people.nearby.model.Person
+import gdg.people.nearby.person.PersonActivity
 import kotlinx.android.synthetic.main.welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
@@ -38,6 +40,12 @@ class WelcomeActivity : AppCompatActivity() {
             }
 
         })
+
+        debug_person.setOnClickListener {
+            val person = Person("birte_id", "Birte", setOf("AI", "Filme", "Sport"))
+            startActivity(Intent(this, PersonActivity::class.java)
+                    .putExtra("person", Gson().toJson(person)))
+        }
     }
 
     private fun getPerson(): Person = preferences.getPerson()
