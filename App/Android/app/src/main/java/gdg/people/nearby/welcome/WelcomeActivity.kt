210dd5ed4884acb994f2601a34445df340978d3a
Marc Reichelt <mcreichelt@gmail.com>
2017-11-18 14:16:10 +0100

    add layout for welcome screen

--- App/Android/app/src/main/java/gdg/people/nearby/welcome/WelcomeActivity.kt
+++ App/Android/app/src/main/java/gdg/people/nearby/welcome/WelcomeActivity.kt
@@ -1,11 +1,14 @@
 package gdg.people.nearby.welcome
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import gdg.people.nearby.R
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome)
     }
+
 }
