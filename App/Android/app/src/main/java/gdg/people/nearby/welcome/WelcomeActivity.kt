c75a3ca1b4a54e40d9d8ac1ff3db0bd38658cf95
Goddchen <goddchen@gmail.com>
2017-11-18 13:49:22 +0100

    initial project import

---
+++ App/Android/app/src/main/java/gdg/people/nearby/welcome/WelcomeActivity.kt
@@ -0,0 +1,11 @@
+package gdg.people.nearby.welcome
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
