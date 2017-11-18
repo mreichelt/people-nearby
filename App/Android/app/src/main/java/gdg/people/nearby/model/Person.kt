5649a5942e4fd3ee6a06ab0895591fd62dfad34c
Marc Reichelt <mcreichelt@gmail.com>
2017-11-18 15:06:02 +0100

    use set, no equals/hashCode functions

--- App/Android/app/src/main/java/gdg/people/nearby/model/Person.kt
+++ App/Android/app/src/main/java/gdg/people/nearby/model/Person.kt
@@ -1,26 +1,3 @@
 package gdg.people.nearby.model
 
-import java.util.*
-
-/**
- * Created by goddc on 18.11.2017.
- */
-data class Person(val name: String, val interests: Array<String>) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Person
-
-        if (name != other.name) return false
-        if (!Arrays.equals(interests, other.interests)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + Arrays.hashCode(interests)
-        return result
-    }
-}
+data class Person(val name: String, val interests: Set<String>)
